540866785b5990767817d313d6e4644f0b794a7d
Diego Miszczuk <diegohmiszczuk@gmail.com>
2023-01-16 11:39:29 -0300

    agrego funcion listado y funcion busqueda por dni

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-// let alumno = "";
+
 let alumnos = [];
  
 
@@ -28,6 +28,7 @@ _materias.push(new Materia("Historia"));
 class Alumno {
   constructor(nombre, dni) {
     this.nombre = nombre;
+    
     this.dni = dni;
   }
   materias = _materias
@@ -48,172 +49,84 @@ const CargarNotas =(dni)=>{
     
   }
 
-  // alert(alumnos[0].materias)
-  // console.log(alumnos)
 
+  const busquedaDni = (dni) =>{
+    
+    if (alumnos.find((el) => el.dni === dni)) {
+      for (let index = 0; index < alumnos.length; index++) {
+        if (dni === alumnos[index].dni ) {
+          alert("El Dni Nro: "+ alumnos[index].dni + " Esta registrado al Alumno: " +alumnos[index].nombre)
+        }
+      }
+     
+    } else {
+      alert(" No se encuentra el Dni consultado.")
+    }
+  }
+
+
+  const listadoAlumnos = () =>{
+    const nombres = alumnos.map((el) => el.nombre)
+    alert("listado de alumnos registrados: " + "\n" + nombres.join(", \n"))
+  };
+
+
+
+    
 const verPromedios =(dni)=>{
   for (let index = 0; index < alumnos.length; index++) {
     if (dni === alumnos[index].dni) {
       for (let i = 0; i < alumnos[index].materias.length; i++) {
         alert("Alumno: "+ alumnos[index].nombre +"\n" + "Calificacion final de " +_materias[i].nombre +": "
-         + alumnos[index].materias[i].promedio(
+         + _materias[index].promedio(
           alumnos[index].materias[i].nota1,
           alumnos[index].materias[i].nota2,
           alumnos[index].materias[i].nota3))
-          
       } 
     }
-    // else if(dni != alumnos[index].dni){
-    //   alert("Lo sentimos, no encontramos alumno con ese DNI")
-    // }
- 
   }
-  
 }
-console.log(_materias)
+
+
+
 
 menuPrincipal();
 
 function menuPrincipal() {
   let opcion = 0;
   let menu =
-    "Bienvenido al Sistema de calificaciones\n Ingrese: \n 1 - Crear Alumno\n 2 - Calificar alumno\n 3 - Ver Promedios\n 4 - Buscar alumno por DNI\n 5 - Buscar alumno por Nombre \n 0 - para salir";
+    "Bienvenido al Sistema de calificaciones\n Ingrese: \n 1 - Crear Alumno\n 2 - Calificar alumno\n 3 - Ver Promedios\n 4 - Buscar alumno por DNI\n 5 - Listado de Alumnos \n 0 - para salir";
   opcion = Number(prompt(menu));
 
   switch (opcion) {
     case 0:
-      alert("Gracias por utilizar el sistema de calificaciones, Hasta pronto!")
-      break;
+        alert("Gracias por utilizar el sistema de calificaciones, Hasta pronto!")
+        break;
     case 1:
-      creaAlumno((prompt("ingrese nombre")).toLowerCase() , Number.parseInt(prompt("ingrese dni")));
-      alert("Alumno creado con exito")
-      menuPrincipal();
-      console.log(alumnos)
-      break;
+        creaAlumno((prompt("ingrese nombre")).toLowerCase() , Number.parseInt(prompt("ingrese dni")));
+        alert("Alumno creado con exito")
+        menuPrincipal();
+      
+        break;
     case 2:
-      CargarNotas(Number.parseInt(prompt("Ingrese el DNI")))
-      // alert("cargar materias para alumno" + alumnos.nombre)
-      menuPrincipal();
-      break;
+        CargarNotas(Number.parseInt(prompt("Ingrese el DNI")))
+        menuPrincipal();
+        break;
     case 3:
-        verPromedios(Number.parseInt.prompt("Ingrese el DNI"))
+        verPromedios(Number.parseInt(prompt("Ingrese el DNI")))
         menuPrincipal()
         break;  
     case 4: 
-    for (let index = 0; index < alumnos.length; index++) {
-      if (alumnos.some((el) => el.dni === Number.parseInt(prompt("ingrese dni sin puntos ni espacios")))) {
-        
-          // alert("El alumno: " +alumnos[index].nombre+ " con DNI: " + alumnos[index].dni + " ,ya se encuentra registrado en el sistema." )
-          alert("El DNI "+ alumnos[index].dni + " Esta registrado al Alumno: " +alumnos[index].nombre)
-          menuPrincipal()  
-        } else {
-          alert("Ingrese solo numeros")
-          menuPrincipal()
-        }
-      }
+        busquedaDni(Number.parseInt(prompt("Ingrese el DNI")))
+        menuPrincipal()
         break;
-      case 5: 
-        for (let index = 0; index < alumnos.length; index++) {
-          if (alumnos.filter((el) => el.nombre.includes(prompt("ingrese Nombre y Apellido").toLowerCase()))){
-            
-               alert("El alumno: " +alumnos[index].nombre+ " con DNI: " + alumnos[index].dni + " ,ya se encuentra registrado en el sistema." )
-              // alert("el DNI "+ alumnos[index].dni + "Esta registrado al Alumno: " +alumnos[index].nombre)
-                
-            } 
-          }
-          break;  
+    case 5: 
+        listadoAlumnos()
+        menuPrincipal()
+        break;  
     
     default:
-      alert("Opcion incorrecta");
+      alert("Opcion incorrecta! ")
       menuPrincipal();
   }
 }
-// console.log(alumnos)
-
-// const resultado = alumnos.find((el) => el.nombre === prompt("ingrese nombre"))
-
-
-
-
-// function creaAlumno(nombre, dni) {
-  
-//   alumnos.push(
-//     new Alumno(
-//       this.nombre = nombre,
-//       this.dni = dni
-//     )
-    
-//   );
-//   // console.log(alumnos)
-//   // console.log(alumnos[0].materias[0].promedio())
-// }
-
-
-
-
-// function menuMaterias() {
-//   let opcion = 0;
-//   let menu =
-//     "Ingrese Materia a calificar\n 1 - Matematicas\n 2 - Fisica\n 3 - Historia\n 4 - Ver boletin\n 5 - Calificar otro alumno\n 0 - para salir";
-
-//   let nota = 0;
-//   let notaMat = 0;
-//   let notaFis = 0;
-//   let notaHis = 0;
-
-//   do {
-//     opcion = Number(prompt(menu));
-//     switch (opcion) {
-//       case 0:
-//         alert("Gracias");
-//         break;
-//       case 1:
-//         notaMat = promedio();
-//         alert(
-//           "La nota final de: " + alumno + " para matematica es: " + notaMat
-//         );
-//         break;
-//       case 2:
-//         notaFis = promedio();
-//         alert("La nota final de: " + alumno + " para Fisica es: " + notaFis);
-//         break;
-//       case 3:
-//         notaHis = promedio();
-//         alert("La nota final de: " + alumno + " para Historia es: " + notaHis);
-//         break;
-//       case 4:
-//         VerBoletin(notaMat, notaFis, notaHis);
-//         break;
-//       case 5:
-//         menuPrincipal();
-//         break;
-//       default:
-//         alert("opcion incorrecta");
-//     }
-//   } while (opcion != 0);
-// }
-
-// function VerBoletin(Pmate, Pfisica, Phistoria) {
-//   alert(
-//     "Alumno: " +
-//       alumno +
-//       "\n" +
-//       "Matematica: " +
-//       Pmate +
-//       "\n" +
-//       "Fisica: " +
-//       Pfisica +
-//       "\n" +
-//       "Historia: " +
-//       Phistoria +
-//       "\n"
-//   );
-// }
-
-// function promedio() {
-//   let nota = 0;
-//   for (let index = 0; index < 3; index++) {
-//     nota = nota + Number(prompt("ingrese nota " + (index + 1)));
-//   }
-//   return (nota / 3).toFixed(2);
-// }
